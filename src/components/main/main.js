6e051d84afc39a03f5e93968ef532602a2ba01da
homePC <vladimirandrosov13@gmail.com>
2022-05-25 21:39:20 +0300

    first

---
+++ src/components/main/main.js
@@ -0,0 +1,16 @@
+
+
+import "./main.css";
+import { TextInput } from "../textInput/textInput";
+import { Button } from "../button/button";
+
+export function Main() {
+   return (
+      <div className="main">
+         <h1>GENDER GUESS</h1>
+         <TextInput />
+         <Button />
+      </div>
+   );
+}
+
---
+++ src/components/textInput/textInput.js
@@ -0,0 +1,7 @@
+import './textInput.css'
+
+export function TextInput() {
+   return (
+      <input type='text' className='textInput' placeholder='Введите имя'></input>
+    );
+};
